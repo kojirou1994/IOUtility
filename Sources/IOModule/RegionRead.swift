81fc57289d980454ce6a297b50b854b3f04a4a8d
kojirou <kojirouhtc@gmail.com>
2022-01-17 21:18:00 +0800

    update RegionRead default implementation
    support ContiguousArray

--- Sources/IOModule/RegionRead.swift
+++ Sources/IOModule/RegionRead.swift
@@ -4,16 +4,29 @@ public protocol RegionRead where Region: Collection, Region.Element == UInt8 {
   mutating func read(upToCount count: Int) throws -> Region
 }
 
-extension RegionRead where Self: Read, Region == [UInt8] {
-  public mutating func read(upToCount count: Int) throws -> Region {
+extension RegionRead where Self: Read {
+  public mutating func read(upToCount count: Int) throws -> Region where Region == [UInt8] {
+    try .init(unsafeUninitializedCapacity: count) { buffer, initializedCount in
+      initializedCount = try read(into: .init(buffer))
+    }
+  }
+
+  public mutating func read(upToCount count: Int) throws -> Region where Region == ContiguousArray<UInt8> {
     try .init(unsafeUninitializedCapacity: count) { buffer, initializedCount in
       initializedCount = try read(into: .init(buffer))
     }
   }
 }
 
-extension RegionRead where Self: Read, Self: Seek, Region == [UInt8] {
-  public mutating func read(exactly count: Int) throws -> Region {
+extension RegionRead where Self: Read, Self: Seek {
+  public mutating func read(exactly count: Int) throws -> Region where Region == [UInt8] {
+    try .init(unsafeUninitializedCapacity: count) { buffer, initializedCount in
+      try readExactlyOrSeekBack(into: .init(buffer))
+      initializedCount = count
+    }
+  }
+
+  public mutating func read(exactly count: Int) throws -> Region where Region == ContiguousArray<UInt8> {
     try .init(unsafeUninitializedCapacity: count) { buffer, initializedCount in
       try readExactlyOrSeekBack(into: .init(buffer))
       initializedCount = count
